2a2022b93bfff158aa58810b799a136b1e9637a1
name <email>
2024-04-10 14:54:37 +0200

    Activité 14

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import DishDetails from "./pages/DishDetails.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import About from "./pages/About.jsx";
 import Layout from "./components/Layout.jsx";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Layout/>}>
           <Route index element={<Home/>} />
           <Route path="/plat/:slug" element={<DishDetails/>}/>
+          <Route path="/about" element={<About/>}/>
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../assets/styles/styleheader.css';
 import logo from '../assets/images/logo.webp';
+import { useLocation } from 'react-router-dom';
+
 
 const Header = () => {
+    const location = useLocation();
     return (
         <header>
             <Navbar className='bg-body-tertiary'>
@@ -19,7 +21,8 @@ const Header = () => {
                     </Navbar.Brand>
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav>
-                            <Nav.Link href="/">Accueil</Nav.Link>
+                            <Nav.Link href="/" active={location.pathname === '/'}>Accueil</Nav.Link>
+                            <Nav.Link href="/about" active={location.pathname === '/about'}>A propos</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
