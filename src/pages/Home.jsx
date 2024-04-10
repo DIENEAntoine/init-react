e611fbdf93cd9dc621fa1a61776152c9184587d6
name <email>
2024-04-10 12:40:19 +0200

    Activité 13

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Dish from '../components/Dish';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -11,25 +12,31 @@ const Home = () => {
             <Container>
                 <Row className="justify-content-center">
                     <Col>
-                        <Dish 
-                            name={"Tacos à l’unit"}
-                            image={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"}
-                            price={"3€"}
-                        />
+                        <Link to="/tacos">
+                            <Dish 
+                                name={"Tacos à l’unit"}
+                                image={"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"}
+                                price={"3€"}
+                            />
+                        </Link>
                     </Col>
                     <Col>
-                        <Dish
-                            name={"Enchiladas"}
-                            image={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"}
-                            price={"12€"}
-                        />
+                        <Link to="/enchiladas">
+                            <Dish
+                                name={"Enchiladas"}
+                                image={"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"}
+                                price={"12€"}
+                            />
+                        </Link>
                     </Col>
                     <Col>
-                        <Dish
-                            name={"Mole poblano"}
-                            image={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"}
-                            price={"15€"}
-                        />
+                        <Link to="/mole-poblano">
+                            <Dish
+                                name={"Mole poblano"}
+                                image={"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"}
+                                price={"15€"}
+                            />
+                        </Link>
                     </Col>    
                 </Row>
             </Container>
